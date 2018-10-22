4bb3ce06f0cfe401fef6feff04789ab0e36bf173
Nathan Patnam <npatnam@oh.hra.com>
2018-10-22 16:53:13 -0400

    file dump

---
+++ Design_Patterns/singleton.py
@@ -0,0 +1,23 @@
+
+
+class Singleton:
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if Singleton.__instance == None:
+            Singleton()
+        return Singleton.__instance
+
+    def __init__(self):
+        if Singleton.__instance != None:
+            raise Exception("This class has already been created once")
+        else:
+            Singleton.__instance = self
+
+
+s = Singleton()
+print(s)
+
+s = Singleton.getInstance()
+print(s)
